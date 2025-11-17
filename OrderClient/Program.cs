c5cabae24263524508a3ae84a18d35a61c7ad282
LoaiSaberC4r <loai.sabir@outlook.com>
2025-11-17 15:44:10 +0200

    Add the client Grpc

--- OrderClient/Program.cs
+++ OrderClient/Program.cs
@@ -1,3 +1,6 @@
+using Grpc.Net.Client;
+using OrderClient.Protos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +24,17 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+var channel = GrpcChannel.ForAddress("https://localhost:7195");
+var client = new OrderService.OrderServiceClient(channel);
 
+var createOrderResponse = await client.CreateOrderAsync(new CreateOrderRequest
+{
+    OrderName = "New Order",
+    Items = { new OrderItem { ItemName = "Item1", Quantity = 2 } }
+});
+
+Console.WriteLine($"Order ID: {createOrderResponse.OrderId}, Status: {createOrderResponse.Status}");
+
+var getOrderResponse = await client.GetOrderAsync(new GetOrderRequest { OrderId = createOrderResponse.OrderId });
+Console.WriteLine($"Order Name: {getOrderResponse.OrderName}, Item: {getOrderResponse.Items[0].ItemName}");
 app.Run();
