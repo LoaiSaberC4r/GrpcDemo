c2d8bed6436683db917e1a1d053c25a8ffd0a2da
LoaiSaberC4r <loai.sabir@outlook.com>
2025-11-17 15:12:47 +0200

    Add project files.

---
+++ OrderService/Services/OrderServiceImpl.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using OrderService.Protos;
+
+namespace OrderService.Services
+{
+    public class OrderServiceImpl : OrderService.Protos.OrderService.OrderServiceBase
+    {
+        public override Task<CreateOrderResponse> CreateOrder(CreateOrderRequest request, ServerCallContext context)
+        {
+            var orderId = Guid.NewGuid().ToString();
+            return Task.FromResult(new CreateOrderResponse
+            {
+                OrderId = orderId,
+                Status = "Created"
+            });
+        }
+
+        public override Task<GetOrderResponse> GetOrder(GetOrderRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GetOrderResponse
+            {
+                OrderId = request.OrderId,
+                OrderName = "Sample Order",
+                Items = { new OrderItem { ItemName = "Item1", Quantity = 2 } }
+            });
+        }
+    }
+}
